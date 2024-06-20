255fb4a7b4ec4346a1ced66a779ac8d5acac9fb9
Román Zamora <roman.zamora@roman.zamora-HT4RW0K91R>
2024-06-20 15:33:24 +0200

    .

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "static_site" {
   bucket = var.bucket_name
 
   website {
@@ -26,14 +26,14 @@ resource "aws_s3_bucket" "example" {
 }*/
 
 resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.static_site.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.static_site.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -47,6 +47,6 @@ resource "aws_s3_bucket_acl" "example" {
     aws_s3_bucket_public_access_block.example,
   ]
 
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.static_site.id
   acl    = "public-read"
 }
