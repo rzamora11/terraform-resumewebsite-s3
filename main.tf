e177a3e9d04acc06fa174f688f6ed1ac7c828f70
Román Zamora <roman.zamora@roman.zamora-HT4RW0K91R>
2024-06-21 09:55:22 +0200

    .

--- main.tf
+++ main.tf
@@ -49,3 +49,14 @@ resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.static_site.id
   acl    = "public-read"
 }
+
+resource "aws_s3_object" "object" {
+  bucket = var.bucket_name
+  key    = "index.html"
+  source = "./main.tf"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("./main.tf")
+}
