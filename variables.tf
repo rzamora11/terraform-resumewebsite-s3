a1582bed8a5db3d5652bee9af6a5ff5c6805c56c
Román Zamora <roman.zamora@roman.zamora-HT4RW0K91R>
2024-06-24 16:40:07 +0200

    .

--- variables.tf
+++ variables.tf
@@ -1,12 +1,19 @@
 variable "region" {
   description = "The AWS region to deploy the S3 bucket in"
   type        = string
-  default     = "eu-west-1"
+  default     = "us-west-2"
 }
 
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
+  default     = "www.romanzamoracarreras.com"
+}
+
+variable "domain_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "www.romanzamoracarreras.com"
 }
 
 variable "website_index_document" {
@@ -19,4 +26,20 @@ variable "website_error_document" {
   description = "The error document for the website"
   type        = string
   default     = "error.html"
+}
+
+
+resource "aws_route_53_zone" "exampleDomain" {
+  name = var.domain_name
+}
+
+resource "aws_route53_record" "exampleDomain-a" {
+  zone_id = aws_route53_zone.exampleDomain.zone_id
+  name    = var.domain_name
+  type    = "A"
+  alias {
+    name                   = aws_s3_bucket.example.website_endpoint
+    zone_id                = aws_s3_bucket.example.hosted_zone_id
+    evaluate_target_health = true
+  }
 }
